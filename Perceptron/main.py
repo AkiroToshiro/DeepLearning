aef241bf7654405f3454198573e7d4968a77248d
AkiroToshiro <MaksPrimachenko@gmail.com>
2021-02-17 18:02:44 +0200

    Кілька електронів у шарі

--- Perceptron/main.py
+++ Perceptron/main.py
@@ -8,12 +8,17 @@ training_inputs.append(np.array([1, 0]))
 training_inputs.append(np.array([0, 1]))
 training_inputs.append(np.array([0, 0]))
 
-labels = np.array([1, 0, 0, 0])
+labels = []
+labels.append(np.array([1, 0]))
+labels.append(np.array([0, 0]))
+labels.append(np.array([0, 0]))
+labels.append(np.array([0, 1]))
 
-perceptron = Perceptron(2)
+perceptron = Perceptron(no_of_inputs=2, no_of_electrons=2)
 perceptron.train(training_inputs, labels)
 
 inputs = np.array([1, 1])
 
 print(perceptron.predict(inputs))
+
 
--- Perceptron/perceptron.py
+++ Perceptron/perceptron.py
@@ -3,22 +3,27 @@ import numpy as np
 
 class Perceptron(object):
 
-    def __init__(self, no_of_inputs, threshold=100, learning_rate=0.01):
+    def __init__(self, no_of_inputs, no_of_electrons=1, threshold=100, learning_rate=0.01):
         self.threshold = threshold
         self.learning_rate = learning_rate
-        self.weights = np.zeros(no_of_inputs + 1)
+        self.weights = []
+        for _ in range(no_of_electrons):
+            self.weights.append(np.zeros(no_of_inputs + 1))
 
     def predict(self, inputs):
-        summation = np.dot(inputs, self.weights[1:]) + self.weights[0]
-        if summation > 0:
-            activation = 1
-        else:
-            activation = 0
-        return activation
+        predict = []
+        for w in self.weights:
+            summation = np.dot(inputs, w[1:]) + w[0]
+            if summation > 0:
+                predict.append(1)
+            else:
+                predict.append(0)
+        return predict
 
     def train(self, training_inputs, labels):
         for _ in range(self.threshold):
             for inputs, label in zip(training_inputs, labels):
-                prediction = self.predict(inputs)
-                self.weights[1:] += self.learning_rate * (label - prediction) * inputs
-                self.weights[0] += self.learning_rate * (label - prediction)
+                for w, l in zip(self.weights, label):
+                    prediction = self.predict(inputs)
+                    w[1:] += self.learning_rate * (l - prediction[0]) * inputs
+                    w[0] += self.learning_rate * (l - prediction[0])
